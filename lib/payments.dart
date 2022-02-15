9f57771548e0473b34faa7edcdedc6e6d4dff95d
shamira13 <98102629+shamira13@users.noreply.github.com>
2022-02-16 01:08:48 +0300

    Update payments.dart

--- lib/payments.dart
+++ lib/payments.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:gighub/rating.dart';
+
+import 'main_drawer.dart';
+
 
 class Payments extends StatelessWidget {
   @override
@@ -11,6 +15,19 @@ class Payments extends StatelessWidget {
         ),
         home: Scaffold(
           appBar: AppBar(
+            leading: IconButton(
+            icon: Icon(
+              Icons.arrow_back,
+              color: Colors.white,
+            ),
+            onPressed: () {
+              Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => MainDrawer()),
+                    );
+            },
+            ),
             title: Text('Payments'),
             centerTitle: true,
           ),
@@ -43,7 +60,83 @@ class Payments extends StatelessWidget {
                       child: Text(
                         'Pay',
                       ),
-                      onPressed: () {},
+                      onPressed: () {
+                      showDialog(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return AlertDialog(
+                              content: Stack(
+                                children: <Widget>[
+                                  Positioned(
+                                    right: -40.0,
+                                    top: -40.0,
+                                    child: InkResponse(
+                                      onTap: () {
+                                        Navigator.of(context).pop();
+                                      },
+                                      child: CircleAvatar(
+                                        backgroundColor: Colors.red,
+                                        child: Icon(Icons.close),
+                                      ),
+                                    ),
+                                  ),
+                                  Form(
+                                    child: Column(
+                                        mainAxisSize: MainAxisSize.min,
+                                        children: <Widget>[
+                                          Padding(
+                                            padding: EdgeInsets.all(8.0),
+                                            child: Text(
+                                              'Would you like to Rate and and give a Review your employee?',
+                                              style: TextStyle(
+                                                  fontWeight: FontWeight.bold),
+                                            ),
+                                          ),
+                                          Padding(
+                                            padding: const EdgeInsets.all(8.0),
+                                            child: ElevatedButton(
+                                              style: ElevatedButton.styleFrom(
+                                                primary: Colors.amber,
+                                              ),
+                                              child: Text('Rate and Review'),
+                                              onPressed: () {
+                                                //
+                                                Navigator.push(
+                                                  context,
+                                                  MaterialPageRoute(
+                                                      builder: (context) =>
+                                                          Test()),
+                                                );
+                                              },
+                                            ),
+                                          ),
+                                          Padding(
+                                            padding: const EdgeInsets.all(8.0),
+                                            child: ElevatedButton(
+                                              style: ElevatedButton.styleFrom(
+                                                primary: Colors.amber,
+                                              ),
+                                              child: Text(
+                                                'Skip',
+                                              ),
+                                              onPressed: () {
+                                                Navigator.push(
+                                                  context,
+                                                  MaterialPageRoute(
+                                                      builder: (context) =>
+                                                          MainDrawer()),
+                                                );
+                                              },
+                                            ),
+                                          )
+                                        ]),
+                                  ),
+                                ],
+                              ),
+                            );
+                          });
+                      },
+                   
                       style: ElevatedButton.styleFrom(
                         shape: new RoundedRectangleBorder(
                           borderRadius: new BorderRadius.circular(60.0),
